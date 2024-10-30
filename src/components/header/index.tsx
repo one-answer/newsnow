369f10b34cd57d8865e0b85f49e7d28c9d6babf8
Oliver <wqdyxnbd@163.com>
2024-10-30 10:36:55 +0800

    update

--- src/components/header/index.tsx
+++ src/components/header/index.tsx
@@ -60,13 +60,13 @@ export function Header() {
       <span className="flex justify-self-start">
         <Link to="/" className="flex gap-2 items-center">
           <div className="h-10 w-10 bg-cover" title="logo" style={{ backgroundImage: "url(/news.svg)" }} />
-          <span className="text-2xl font-brand line-height-none!">
-            <p>刷一下</p>
-            {/* <p className="mt--1"> */}
-            {/*  <span className="color-primary-6">N</span> */}
-            {/*  <span>ow</span> */}
-            {/* </p> */}
-          </span>
+          {/* <span className="text-2xl font-brand line-height-none!"> */}
+          {/*  <p>刷一下</p> */}
+          {/*   <p className="mt--1"> */}
+          {/*    <span className="color-primary-6">N</span> */}
+          {/*    <span>ow</span> */}
+          {/*   </p> */}
+          {/* </span> */}
         </Link>
         {/* <a target="_blank" href={`${Homepage}/releases/tag/v${Version}`} className="btn text-sm ml-1 font-mono"> */}
         {/*  {`v${Version}`} */}
