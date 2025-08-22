9956146a3019af631da1714b7aafbe19aba52c84
zhaoyuedi <307990280@qq.com>
2025-08-22 13:40:45 +0800

    接口

--- server/sources/xueqiu.ts
+++ server/sources/xueqiu.ts
@@ -15,7 +15,7 @@ interface StockRes {
 
 const hotstock = defineSource(async () => {
   const url = "https://stock.xueqiu.com/v5/stock/hot_stock/list.json?size=30&_type=10&type=10"
-  const cookie = (await $fetch.raw("https://xueqiu.com/?md5__1038=2840ce7d37-aYX55duZaZSsUjYylbXSduLaaqeapwO3CucYl2sXdRXXuS5UXKdvUPvXsXxuF0XylLuasuIuvq0u8uubDuBKiwuAKSXvudZuDyUayBt05ZirucziDiUy3ul%2Fs3iydqzXwt0yPgUT0msi75VC5yVscAuDdAaCwA5Rl5yilSwu2FXbathyQzu")).headers.getSetCookie()
+  const cookie = (await $fetch.raw("https://xueqiu.com/?md5__1038=28178cb220-kS861Td6gTw0elSUixTppTtxSUk%2F6pTZIbXdiHSTOkT26MSJx72JXppGW8SY%2F2TJrTI6T4Tv6HM%2FT2UPITPUJBT7UPziTliTdT1iJnh37M%3DP6t3TWTlSTsTicTtTTRTTi6J0TD6HeTlIKlThZ63kHvTOUJRTMczgIcC07iS07UlOpp%2FmISlJgZvGQry0gyGmsyviT")).headers.getSetCookie()
   const res: StockRes = await $fetch(url, {
     headers: {
       cookie: cookie.join("; "),
